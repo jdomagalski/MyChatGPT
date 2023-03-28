e5070f539e85e6679e8686e993cb4691ca891573
jdomagalski <jdomagal@umich.edu>
2023-03-28 17:24:38 -0400

    Create main.py
    initializing file from template

---
+++ main.py
@@ -0,0 +1,25 @@
+from llama_index import GPTSimpleVectorIndex, Document, SimpleDirectoryReader
+import os
+
+os.environ['OPENAI_API_KEY'] = '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'
+
+
+# Loading from a directory
+documents = SimpleDirectoryReader('your_directory').load_data()
+
+# Loading from strings, assuming you saved your data to strings text1, text2, ...
+text_list = [text1, text2, ...]
+documents = [Document(t) for t in text_list]
+
+
+# Construct a simple vector index
+index = GPTSimpleVectorIndex(documents)
+
+# Save your index to a index.json file
+index.save_to_disk('index.json')
+# Load the index from your saved index.json file
+index = GPTSimpleVectorIndex.load_from_disk('index.json')
+
+# Querying the index
+response = index.query("What features do users want to see in the app?")
+print(response)
